3ca27431ebaf2e10917d5bb41871ce0d59f56835
BrunoTerres <brunoeterres@gmail.com>
2021-10-23 19:22:29 -0300

    snippets app added to tutorial settingsC

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -42,7 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rest_framework',
+    'snippets.apps.SnippetsConfig',
 ]
 
 MIDDLEWARE = [
