c354a14d72d42840be7f1b5059c93d008b7bbb04
BrunoTerres <brunoeterres@gmail.com>
2021-10-25 18:43:17 -0300

    Refactoring view and adding @api_view decorator

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
     path('', views.snippet_list),
     path('<int:pk>/', views.snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
