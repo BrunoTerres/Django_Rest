aec0751b4353e6a3ac3c70b28879bddb08633762
BrunoTerres <brunoeterres@gmail.com>
2021-10-27 18:55:41 -0300

    class based view

--- snippets/urls.py
+++ snippets/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
-    path('', views.snippet_list),
-    path('<int:pk>/', views.snippet_detail),
+    path('', views.SnippetList.as_view()),
+    path('<int:pk>/', views.SnippetDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
